32bfc77e172bec88f7fbb6fe13746397b0f04bbe
LordMusse <rasmus@feltzing.se>
2026-02-02 23:31:18 +0100

    fix(deploy scripts): fixed so that the raspberry pi don't receive git blob

--- upload_to_pi.sh
+++ upload_to_pi.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 #  <<< acts as the stdin i.e. user input
-
+GLOBIGNORE=".git"
 # $0 är kommandot själv $1 är första argumentet osv upp till $9
 location=$1
 #om jag är hemma
 if [[ $location == "h" ]]; then
-  sftp amalsthai@192.168.1.41:/home/amalsthai/mjukvara <<< $'put -r ./'
+  sftp amalsthai@192.168.1.41:/home/amalsthai/mjukvara <<< $'put -r ./*'
 # eller om jag kopplar direkt via ethernet
 elif [[ $location == "e" ]]; then
-  sftp amalsthai@192.168.1.100:/home/amalsthai/mjukvara <<< $'put -r ./'
+  sftp amalsthai@192.168.1.100:/home/amalsthai/mjukvara <<< $'put -r ./*'
 else
   echo "lägg till argumentet 'h' om du är hemma eller 'e' om du kopplar direkt via ethernet/local-dhcp"
 fi
